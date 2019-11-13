172f9c2d92573069639bf4f7daca46a8c89453da
jay <cleve703@gmail.com>
2019-11-13 09:58:54 -0500

    save and load functions working properly

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-require 'yaml'
+# frozen_string_literal: true
 
 class String
   def colorize(color_code)
@@ -15,10 +15,11 @@ class String
 end
 
 class Hangman
+  require 'yaml'
 
   def initialize
     @guess_array = []
-    @letters = ("A".."Z").to_a
+    @letters = ('A'..'Z').to_a
     @misses = 0
     play_game
   end
@@ -26,17 +27,17 @@ class Hangman
   def play_game
     new_or_saved
   end
-  
+
   def new_or_saved
-    print %Q(
-      Welcome to Hangman.  Would you like to start a 
+    print %(
+      Welcome to Hangman.  Would you like to start a
       NEW game or LOAD a SAVED game?
-      
+
       1 = NEW game
       2 = LOAD a SAVED game
       3 = QUIT
-      
-      Enter 1 or 2: )
+
+      Enter 1, 2 or 3: )
     valid_answer = false
     while valid_answer == false
       @new_or_saved = gets.chomp.to_i
@@ -47,32 +48,30 @@ class Hangman
         game_sequence
       elsif @new_or_saved == 2
         valid_answer = true
-        load_saved
+        saved_games_menu
       elsif @new_or_saved == 3
         break
-      else print %Q(You may only enter 1, 2 or 3.  Try again.)
+      else print %(You may only enter 1, 2 or 3.  Try again.)
       end
     end
-  end    
-  
+  end
+
   def generate_word
-    wordlist = File.open("lib/5desk.txt", "r")
+    wordlist = File.open('lib/5desk.txt', 'r')
     valid_word = false
     while valid_word == false
       random_num = rand(wordlist.readlines.size)
       @solution_word = File.readlines(wordlist)[random_num].strip.upcase
-      if (5..12).to_a.include?(@solution_word.length)
-        valid_word = true
-      end
+      valid_word = true if (5..12).to_a.include?(@solution_word.length)
     end
     wordlist.close
     puts @solution_word
-    (@solution_word.length).times {@guess_array.push("_ ")}
-    @solution_array = @solution_word.split("")
+    @solution_word.length.times { @guess_array.push('_ ') }
+    @solution_array = @solution_word.split('')
   end
 
   def explain_rules
-    print %Q(
+    print %(
       In the game of "Hangman," you are given an
       unknown word with a known length, and your
       objective is to guess what the word is.
@@ -86,98 +85,134 @@ class Hangman
 
   def game_sequence
     if @misses == 6
-      print "You lost! Press [ENTER] to play again or [X] to exit."
+      print 'You lost! Press [ENTER] to play again or [X] to exit.'
       replay_question
     end
     while @misses < 6
       scoreboard
       get_guess
-      if @guess_array == @solution_array
-        scoreboard
-        @misses = 6
-        print "You won! Press [ENTER] to play again or [X] to exit."
-        replay_question
-      end
+      next unless @guess_array == @solution_array
+
+      scoreboard
+      @misses = 6
+      print 'You won! Press [ENTER] to play again or [X] to exit.'
+      replay_question
     end
   end
 
   def scoreboard
-    print %Q(
-      
-      #{@misses} miss\(es\) out of 6 total.
-    
-      #{@guess_array.join(" ")}  #{@letters.join("")}
-    
-      )
+    print %(
 
+      #{@misses} miss\(es\) out of 6 total.
+
+      #{@guess_array.join(' ')}  #{@letters.join('')}
+
+      )
   end
 
   def get_guess
-    print %Q(
+    print %(
       Enter a guess, or type SAVE to save your game: )
     guess = gets.chomp.upcase
     validate_guess(guess)
   end
 
   def validate_guess(guess)
-    case
-      when guess == "SAVE"
-        save_game  
-      when guess.length != 1
-        puts %Q(
-      You may only guess one character at a time.  Try again.
-          )
-        get_guess
-      when !@letters.include?(guess)
-        puts %Q(
-      You can only enter a letter.  Try again.
-          )
+    if guess == 'SAVE'
+      save_game
+    elsif guess.length != 1
+      puts %(
+        You may only guess one character at a time.  Try again.
+        )
+      get_guess
+    elsif @guess_array.include?(guess)
+      puts %(
+        You have already guessed that letter.  Try again.
+        )
+    elsif !@letters.include?(guess)
+      puts %(
+        You can only enter a letter.  Try again.
+        )
     else
       process_guess(guess)
+      end
     end
-  end
 
   def process_guess(guess)
     if @solution_array.include?(guess)
       @solution_array.each_with_index do |letter, index|
         @guess_array[index] = guess if letter == guess
       end
-      @letters.map! do |letter| 
-        if letter == guess.upcase
-          letter = letter.green
-        else
-          letter = letter
-        end
+      @letters.map! do |letter|
+        letter = if letter == guess.upcase
+                   letter.green
+                 else
+                   letter
+                 end
       end
     else
       @misses += 1
-      @letters.map! do |letter| 
-        if letter == guess.upcase
-          letter = letter.red
-        else
-          letter = letter
-        end
+      @letters.map! do |letter|
+        letter = if letter == guess.upcase
+                   letter.red
+                 else
+                   letter
+                 end
       end
     end
-  end
+   end
 
   def replay_question
     play_again = gets.chomp.upcase
-    if play_again == ""
-      Hangman.new
-    end
+    Hangman.new if play_again == ''
   end
 
   def save_game
     time = Time.new
-    default_filename = time.strftime("%Y%m%d%H%m%S")
-    puts "Default filename is #{default_filename}"
-    puts self.inspect
-    File.open("#{default_filename}.bin", "wb") {|f| f.write(Marshal.dump(self))}
+    default_filename = time.strftime('%Y%m%d%H%m%S')
+    game_data = {}
+    game_data[:solution_word] = @solution_word
+    game_data[:misses] = @misses
+    game_data[:guess_array] = @guess_array
+    game_data[:letters] = @letters
+    game_data[:solution_array] = @solution_array
+    puts game_data
+    File.open("./yaml/#{default_filename}.yml", 'w') { |f| f.write(game_data.to_yaml) }
   end
 
-  def load_saved
-    puts "Loading saved game..."
+  def saved_games_menu
+    saved_games_hash = {}
+    saved_games = Dir['./yaml/*.yml'].to_a
+    saved_games.map!.with_index do |f, index|
+      saved_games_hash[index + 1] = f
+    end
+    saved_games_readable = []
+    saved_games_hash.each do |key, value|
+      value = value.sub('./yaml/', '')
+      value = value.sub('.yml', '')
+      saved_games_readable.push("#{key}: #{value}")
+    end
+    print %(
+            Select the number corresponding to your saved game
+            from the list below:
+
+            #{saved_games_readable}
+
+            Enter a number: )
+    selected_game = gets.chomp.to_i
+    selected_game = saved_games_hash.values_at(selected_game)
+    load_game(selected_game)
+    end
+
+  def load_game(selected_game)
+    selected_game = selected_game.join('')
+    game_data = YAML.load_file(selected_game)
+    @solution_word = game_data.values_at(:solution_word).join('')
+    @misses = game_data.values_at(:misses).join('').to_i
+    @guess_array = game_data.values_at(:guess_array).flatten
+    @letters = game_data.values_at(:letters).flatten
+    @solution_array = @solution_word.split('')
+    game_sequence
   end
 
 end
